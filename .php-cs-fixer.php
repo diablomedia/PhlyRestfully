869bb5d56888553c4238c487c2ef9db319c299b7
Jay Klehr <jay@diablomedia.com>
2022-02-01 15:45:55 -0700

    fixing php-cs-fixer, adding merge-me workflow

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,20 +6,19 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__ . '/test')
 ;
 
-return PhpCsFixer\Config::create()
-    ->setRules([
-        '@PSR2' => true,
-        '@PHP70Migration' => true,
-        '@PHP70Migration:risky' => true,
-        '@PHP71Migration' => true,
-        '@PHP71Migration:risky' => true,
-        '@PHPUnit60Migration:risky' => true,
-        '@PHPUnit75Migration:risky' => true,
-        '@PHPUnit84Migration:risky' => true,
-        'no_whitespace_in_blank_line' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'declare_strict_types' => false,
-    ])
-    ->setRiskyAllowed(true)
-    ->setFinder($finder)
-;
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    '@PSR2' => true,
+    '@PHP70Migration' => true,
+    '@PHP70Migration:risky' => true,
+    '@PHP71Migration' => true,
+    '@PHP71Migration:risky' => true,
+    '@PHPUnit60Migration:risky' => true,
+    '@PHPUnit75Migration:risky' => true,
+    '@PHPUnit84Migration:risky' => true,
+    'no_whitespace_in_blank_line' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'declare_strict_types' => false,
+])
+->setRiskyAllowed(true)
+->setFinder($finder);
